dce047fd115d7e11103c1bc5dac2d1aad86a7f2e
arjun963 <arjun.bhattrai.7@gmail.com>
2024-02-05 23:12:40 +0530

    Adding for first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import video from "../src/assets/video.mp4";
 
-function App() {
+function Login() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <video src={video} autoPlay loop muted />
+      <div className="wrapper">
+        <form action="#">
+          <div className="title">Login</div>
+
+          <div className="field">
+            <input type="text" required />
+            <label>Email Address</label>
+          </div>
+          <div className="field">
+            <input type="password" required />
+            <label>Password</label>
+          </div>
+          <div className="content">
+            <div className="checkbox">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            <div className="pass-link">
+              <a href="#">Forgot password?</a>
+            </div>
+          </div>
+          <div className="field">
+            <input type="submit" value="Login" />
+          </div>
+          <div className="signup-link">
+            Not a member? <a href="#">Signup now</a>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Login;
